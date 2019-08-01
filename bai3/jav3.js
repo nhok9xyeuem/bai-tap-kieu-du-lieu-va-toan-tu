05e6299c9ee3ba728a68c36d49184a54ea8b09ae
Nguyễn Như Công <nhuconghaui@gmail.com>
2019-08-01 13:41:04 +0700

    update bai tap

---
+++ bai3/jav3.js
@@ -0,0 +1,4 @@
+let r = prompt("moi ban nhap ban kinh");
+r=parseFloat(r);
+S=r*r*3.14 ;
+document.write("dien tich hinh tron la :"+S)
---
+++ bai4/bai4.js
@@ -0,0 +1,4 @@
+let r = prompt("moi ban nhap ban kinh");
+r=parseFloat(r);
+S=r*2*3.14 ;
+document.write("chu vi hinh tron la :"+S)
